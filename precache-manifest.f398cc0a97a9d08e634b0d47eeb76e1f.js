02a26a89b7490571b5750a51ac7a20eddb295627
manavvp08 <manavp080@gmail.com>
2024-03-25 17:13:53 +0530

    Updates

---
+++ precache-manifest.f398cc0a97a9d08e634b0d47eeb76e1f.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57a4637f885eba213f8601ddfa32ca99",
+    "url": "/ManavPurswaniPortfolio/index.html"
+  },
+  {
+    "revision": "1638eebc5752ec171eeb",
+    "url": "/ManavPurswaniPortfolio/static/css/2.69233f45.chunk.css"
+  },
+  {
+    "revision": "67654c956e5ee2fdf47f",
+    "url": "/ManavPurswaniPortfolio/static/css/main.ea34a9c3.chunk.css"
+  },
+  {
+    "revision": "1638eebc5752ec171eeb",
+    "url": "/ManavPurswaniPortfolio/static/js/2.1e27345b.chunk.js"
+  },
+  {
+    "revision": "f008d04feea7167e0c6b6faca8c765ff",
+    "url": "/ManavPurswaniPortfolio/static/js/2.1e27345b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67654c956e5ee2fdf47f",
+    "url": "/ManavPurswaniPortfolio/static/js/main.10884140.chunk.js"
+  },
+  {
+    "revision": "717c4afd0cbf6f9fce4b",
+    "url": "/ManavPurswaniPortfolio/static/js/runtime-main.63f0f38b.js"
+  },
+  {
+    "revision": "36b4ba6db6ec415fd91e435d8a824ea6",
+    "url": "/ManavPurswaniPortfolio/static/media/bubbles-together.36b4ba6d.png"
+  },
+  {
+    "revision": "20e3ec16fb55fe4b60aa4c08ae31bf5c",
+    "url": "/ManavPurswaniPortfolio/static/media/bubblify.20e3ec16.png"
+  },
+  {
+    "revision": "00dcd0a9d835cccea1200f16651e3f66",
+    "url": "/ManavPurswaniPortfolio/static/media/color-game.00dcd0a9.png"
+  },
+  {
+    "revision": "167189ab8c372f329e3b618e7a20f351",
+    "url": "/ManavPurswaniPortfolio/static/media/daniel-profile.167189ab.jpg"
+  },
+  {
+    "revision": "0db173e48cea39a077c61c0cfbc92ec0",
+    "url": "/ManavPurswaniPortfolio/static/media/debug-duck.0db173e4.png"
+  },
+  {
+    "revision": "93a54274ef1ca03490f0e8a06a7be57d",
+    "url": "/ManavPurswaniPortfolio/static/media/fractaltree.93a54274.png"
+  },
+  {
+    "revision": "68ff37b5a7e526717fcd857cbdb09f09",
+    "url": "/ManavPurswaniPortfolio/static/media/icons.68ff37b5.svg"
+  },
+  {
+    "revision": "3648b303373f846b7e8c4f4c456ebf7f",
+    "url": "/ManavPurswaniPortfolio/static/media/pong.3648b303.png"
+  },
+  {
+    "revision": "16dd7b3ac4d6c0e0e3cb2e918127e435",
+    "url": "/ManavPurswaniPortfolio/static/media/puzzle-game.16dd7b3a.PNG"
+  },
+  {
+    "revision": "5b0cfaf179df734ae43adf0221a7c392",
+    "url": "/ManavPurswaniPortfolio/static/media/spiral-orbit.5b0cfaf1.png"
+  },
+  {
+    "revision": "b0e69915387564249f34506e63985ecf",
+    "url": "/ManavPurswaniPortfolio/static/media/tankmania.b0e69915.png"
+  },
+  {
+    "revision": "a0bd69978b60db67f00b88f42364193d",
+    "url": "/ManavPurswaniPortfolio/static/media/todo-list.a0bd6997.png"
+  },
+  {
+    "revision": "1e604d450e8d55c7badbeaff18ea2f05",
+    "url": "/ManavPurswaniPortfolio/static/media/website.1e604d45.png"
+  }
+]);
